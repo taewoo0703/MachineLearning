9d60514ee204486b700d31a79b59505d36b63631
taewoo0703 <taewoo0703@naver.com>
2021-02-18 03:01:43 +0900

    Ch3.3.4완료 p141

--- Ch3.py
+++ Ch3.py
@@ -1,5 +1,5 @@
 ### MNIST 데이터셋
-##데이터셋 내려받기
+## 데이터셋 내려받기
 from sklearn.datasets import fetch_openml
 mnist = fetch_openml('mnist_784', version=1)
 mnist.keys()
@@ -20,3 +20,120 @@ import numpy as np
 y = y.astype(np.uint8)
 #훈련데이터, 테스트데이터 나누기
 X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+### 2진 분류기 훈련
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+## 확률적 경사하강법
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
+
+### 성능 측정
+## 교차검증
+#교차검증 만들기 폴드 3개짜리
+from sklearn.model_selection import StratifiedKFold
+from sklearn.base import clone
+
+skfolds = StratifiedKFold(n_splits=3, random_state=42, shuffle=True)
+
+for train_index, test_index in skfolds.split(X_train, y_train_5):
+    clone_clf = clone(sgd_clf)
+    X_train_folds = X_train[train_index]
+    y_train_folds = y_train_5[train_index]
+    X_test_fold = X_train[test_index]
+    y_test_fold = y_train_5[test_index]
+
+    clone_clf.fit(X_train_folds, y_train_folds)
+    y_pred = clone_clf.predict(X_test_fold)
+    n_correct = sum(y_pred == y_test_fold)
+    print(n_correct / len(y_pred))
+
+#교차검증 cross_val_score() 사용
+from sklearn.model_selection import cross_val_score
+cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+#5아님 더미분류기 생성
+from sklearn.base import BaseEstimator
+class Never5Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        return self
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
+#accuracy 지표의 함정
+never_5_clf = Never5Classifier()
+cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+## 오차행렬
+#cross_val_predict() 사용
+from sklearn.model_selection import cross_val_predict
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+#confusion_matrix() 사용해 오차행렬 생성
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_train_5, y_train_pred)
+
+## 정밀도와 재현율
+from sklearn.metrics import precision_score, recall_score
+precision_score(y_train_5, y_train_pred) #정밀도
+recall_score(y_train_5, y_train_pred) #재현율
+
+#F1 score : 정밀도와 재현율의 조화평균
+from sklearn.metrics import f1_score
+f1_score(y_train_5, y_train_pred)
+
+## 정밀도/재현율 트레이드오프
+#decision_function()으로 각 샘플의 점수 확인
+y_scores = sgd_clf.decision_function([some_digit])
+y_scores
+threshold = 9000
+y_some_digit_pred = (y_scores > threshold)
+
+#모든 샘플의 점수 구하기
+y_scores = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3,
+                             method="decision_function")
+#precision_recall_curve
+from sklearn.metrics import precision_recall_curve
+precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+def plot_precision_recall_vs_threshold(precisions, recalls, thresholds):
+    plt.plot(thresholds, precisions[:-1], "b--", label="정밀도")
+    plt.plot(thresholds, recalls[:-1], "g-", label="재현율")
+    plt.legend(loc="center right", fontsize=16)
+    plt.xlabel("Threshold", fontsize=16)
+    plt.grid(True)
+    plt.axis([-50000, 50000, 0, 1])
+
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+
+recall_90_precision = recalls[np.argmax(precisions >= 0.90)]
+threshold_90_precision = thresholds[np.argmax(precisions >= 0.90)]
+
+plt.figure(figsize=(8, 4))                                                                  # Not shown
+plot_precision_recall_vs_threshold(precisions, recalls, thresholds)
+plt.plot([threshold_90_precision, threshold_90_precision], [0., 0.9], "r:")                 # Not shown
+plt.plot([-50000, threshold_90_precision], [0.9, 0.9], "r:")                                # Not shown
+plt.plot([-50000, threshold_90_precision], [recall_90_precision, recall_90_precision], "r:")# Not shown
+plt.plot([threshold_90_precision], [0.9], "ro")                                             # Not shown
+plt.plot([threshold_90_precision], [recall_90_precision], "ro")                             # Not shown
+plt.show()
+
+#재현율-정밀도 곡선 그리기
+def plot_precision_vs_recall(precisions, recalls):
+    plt.plot(recalls, precisions, "b-", linewidth=2)
+    plt.xlabel("Recall", fontsize=16)
+    plt.ylabel("Precision", fontsize=16)
+    plt.axis([0, 1, 0, 1])
+    plt.grid(True)
+plt.figure(figsize=(8, 6))
+plot_precision_vs_recall(precisions, recalls)
+plt.plot([recall_90_precision, recall_90_precision], [0., 0.9], "r:")
+plt.plot([0.0, recall_90_precision], [0.9, 0.9], "r:")
+plt.plot([recall_90_precision], [0.9], "ro")
+#정밀도 90%인 쓰레쉬홀드에 대하여 재현율 계산
+threshold_90_precision = thresholds[np.argmax(precisions >= 0.90)]
+y_train_pred_90 = (y_scores >= threshold_90_precision)
+precision_score(y_train_5, y_train_pred_90)
+recall_score(y_train_5, y_train_pred_90)
+
+### ROC 곡선
